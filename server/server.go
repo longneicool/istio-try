1082957d200b662eb6a83145a64a63191ef0cace
dylanz <dylan.zhang@nokia-sbell.com>
2019-08-07 21:49:18 +0800

    Add the http server

--- server/server.go
+++ server/server.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"html"
 	"log"
 	"net"
+	"net/http"
 	"os"
 
 	istiotest "github.com/longneicool/istio-try"
@@ -13,6 +15,21 @@ import (
 )
 
 type server struct{}
+
+type myHandler struct{}
+
+func (m *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Rcv message from %s", r.URL.Path)
+	fmt.Fprintf(w, "Rcv the request from %s", r.URL.Path)
+}
+
+func ListeningHttp() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":9091", nil))
+}
 
 func (s *server) SendMessage(ctx context.Context, in *istiotest.Request) (*istiotest.Reply, error) {
 	fmt.Printf("Rcv message from %s:%s\n", in.GetName(), in.GetMessage())
@@ -24,9 +41,12 @@ func (s *server) SendMessage(ctx context.Context, in *istiotest.Request) (*istio
 }
 
 func main() {
+
 	ip := flag.String("ip", "127.0.0.1", "The listener ip of the server")
-	port := flag.String("prot", "50001", "the listener port")
+	port := flag.String("prot", "9092", "the listener port")
 	flag.Parse()
+
+	go ListeningHttp()
 
 	name := os.Getenv("SERVER_NAME")
 	if len(name) == 0 {
